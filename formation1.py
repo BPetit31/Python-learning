8c155fd41e0ffce0f11e379f6ae407acbd8423f4
BPetit31 <71259894+BPetit31@users.noreply.github.com>
2020-09-14 16:03:56 +0200

    Add files via upload

--- formation1.py
+++ formation1.py
@@ -1,6 +1,4 @@
 # Test program from "Learn how to program in Python" - Openclassrooms
-nb = 4
-i = 0
-while i < 10:
-    print(i + 1, "*", nb, "=", (i + 1) * nb)
-    i += 1
+def fonc(a=1, b=2, c=3, d=4, e=5):
+    print("a =", a,"b =", b, "c =", c, "d =", d, "e =", e)
+fonc(a = 6, c = 12)
