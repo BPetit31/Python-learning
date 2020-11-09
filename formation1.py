f85b6a7d008697d2642b13e6f0feb45c53361b93
BPetit31 <71259894+BPetit31@users.noreply.github.com>
2020-11-09 03:01:10 +0100

    Delete formation1.py
    Nettoyage

--- formation1.py
+++
@@ -1,4 +0,0 @@
-# Test program from "Learn how to program in Python" - Openclassrooms
-def fonc(a=1, b=2, c=3, d=4, e=5):
-    print("a =", a,"b =", b, "c =", c, "d =", d, "e =", e)
-fonc(a = 6, c = 12)
