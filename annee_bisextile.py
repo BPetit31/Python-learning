c5a51386ff628759617ddd6aa4f17327000a08f4
BPetit31 <71259894+BPetit31@users.noreply.github.com>
2020-11-09 03:00:43 +0100

    Delete annee_bisextile.py
    Nettoyage

--- annee_bisextile.py
+++
@@ -1,8 +0,0 @@
-# Test program from "Learn how to program in Python" - Openclassrooms
-annee = input("Saisissez une année : ")
-annee = int(annee)
-if annee % 400 == 0 or (annee % 4 == 0 and annee % 100 != 0):
-    print("Cette année est bisextile.")
-else:
-    print("Ce n'est pas une année bisextile.")
-    
